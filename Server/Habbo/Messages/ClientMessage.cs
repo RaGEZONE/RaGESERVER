eef0cb18386aa87982440f7bcfbf56c7da53ea6b
AWA <audun@null.no>
2012-07-16 21:15:47 +0200

    Message parsing/constructing added

---
+++ Server/Habbo/Messages/ClientMessage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RageServer.Server.Habbo.Messages
+{
+    /// <summary>
+    /// A class to parse client messages (client->server)
+    /// </summary>
+    class ClientMessage
+    {
+        private short _headerID;
+        private byte[] _content;
+        private short _reader;
+
+        public short HeaderID { get { return _headerID; } }
+
+        public static ClientMessage Parse(byte[] data)
+        {
+            ClientMessage request = new ClientMessage();
+            request._content = data;
+            request._headerID = request.ReadInt16();
+
+            return request;
+        }
+        public string ReadString()
+        {
+            short length = ReadInt16();
+            string s = Encoding.UTF8.GetString(_content, _reader, length);
+            _reader += length;
+
+            return s;
+        }
+        public int ReadInt32()
+        {
+            int i = BitConverter.ToInt32(new[] { _content[_reader + 3], _content[_reader + 2], _content[_reader + 1], _content[_reader] }, 0);
+            _reader += 4;
+            return i;
+        }
+        public short ReadInt16()
+        {
+            short s = BitConverter.ToInt16(new[] { _content[_reader + 1], _content[_reader] }, 0);
+            _reader += 2;
+            return s;
+        }
+        public bool ReadBoolean()
+        {
+            return BitConverter.ToBoolean(_content, _reader++);
+        }
+    }
+}
---
+++ Server/Habbo/Messages/ServerMessage.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RageServer.Server.Habbo.Messages
+{
+    /// <summary>
+    /// A class to construct server messages (server->client)
+    /// </summary>
+    class ServerMessage
+    {
+        private List<byte> _builder;
+
+        public ServerMessage(short messageID)
+        {
+            Init(messageID);
+        }
+        public ServerMessage()
+        {
+            _builder = new List<byte>();
+        }
+        public void Init(short messageID)
+        {
+            _builder = new List<byte>();
+            AppendInt16(messageID);
+        }
+
+        public void Append(object o)
+        {
+            _builder.AddRange(Encoding.UTF8.GetBytes(o.ToString()));
+        }
+        public void AppendChar(int i)
+        {
+            Append(Convert.ToChar(i));
+        }
+        public void AppendByte(byte b)
+        {
+            _builder.Add(b);
+        }
+        public void AppendString(string s)
+        {
+            _builder.AddRange(Reorder(BitConverter.GetBytes((short)s.Length)));
+            Append(s);
+        }
+        public void AppendInt32(int i)
+        {
+            byte[] b = BitConverter.GetBytes(i);
+            Array.Reverse(b);
+            _builder.AddRange(b);
+        }
+        public void AppendInt16(short i)
+        {
+            byte[] b = BitConverter.GetBytes(i);
+            Array.Reverse(b);
+            _builder.AddRange(b);
+        }
+        public void AppendBoolean(bool b)
+        {
+            _builder.Add(b ? (byte)1 : (byte)0);
+        }
+
+        public List<byte> ToBytes()
+        {
+            List<byte> sendMessage = new List<byte>();
+
+            sendMessage.AddRange(BitConverter.GetBytes(_builder.Count));
+            sendMessage.Reverse();
+            sendMessage.AddRange(_builder);
+            return sendMessage;
+        }
+        public bool HasContent()
+        {
+            return _builder.Count > 2;
+        }
+        private static byte[] Reorder(byte[] b)
+        {
+            Array.Reverse(b);
+            return b;
+        }
+    }
+}
